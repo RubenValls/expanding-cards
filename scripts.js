8db96d95348703c65aa4b68d40f035c7b987d73f
Rubén Valls <r.vallsaparici@gmail.com>
2023-08-14 18:10:09 +0200

    responsive and interactive gallery finished

--- scripts.js
+++ scripts.js
@@ -1,10 +1,17 @@
 const images = document.querySelectorAll(".card");
+const titles = document.querySelectorAll("h2");
 
-images.forEach((image) => {
+images.forEach((image, index) => {
     image.addEventListener("click", function () {
-        images.forEach((image) => {
+        images.forEach((image, index) => {
             image.classList.remove("active");
+            titles[index].style.visibility = "hidden";
         })
         image.classList.add("active");
+        titles[index].style.visibility = "visible";
     });
+
+    if(image.classList.contains("active")){
+        titles[index].style.visibility = "visible";
+    }
 });
